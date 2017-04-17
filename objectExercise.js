930e25a5bb58c1f74538dc0ea78524cdb1e515fb
James Freeman <freeman.james.h@gmail.com>
2017-04-17 12:44:29 -0600

    initial commit

---
+++ objectExercise.js
@@ -0,0 +1,46 @@
+// create an empty object literal named 'james'
+
+// using dot notation, give james the property 'firstName' with a value of "James"
+
+// using bracket notation, give james the property 'lastName' with a value of "Freeman"
+
+// using bracket notation to access the james object's firstName property, log his firstName
+// using dot notation to access the james object's lastName property, log his last name
+
+// create a new object literal named 'sashy' with the following properties/methods :
+// 1. a 'firstName' property, with the value of "Sasha"
+// 2. a 'lastName property', with the value of "Dunbar"
+// 3. a 'saysHello' method, which when called will log "Hello!"
+// these should all be created inside of the object literal, ie don't use
+// dot or bracket notation to add properties after you've created the object.
+
+// call the sashy object's 'saysHello' method
+
+// for the problems below, you can use whatever notation you prefer (dot vs bracket)
+
+// give the james object an age property and set it to 28
+
+// give the james object a 'hasBirthday' method. when called, this method should
+// add 1 year to james's age AND log the following message:
+// "Happy birthday! James is now 29". However, the age should refer to his current age,
+// so when called again, it should log "Happy Birthday! James is now 30", etc, etc.
+
+// give sashy a 'favoritePeople' property, and set it to an array which contains the names
+// of five of her favorite people
+
+//log the third person in this array, accessing it through the sashy object
+
+//add a new friend, "Crabby Crustacean" to sashy's friend list
+
+//come to think of it, sashy makes friends easily. she should probably have a method
+// called 'makesFriend' that takes a new friend's name as an argument, and adds it
+// to her favoritePeople list
+
+//add a friend to sashy's favoritePeople list using the method you just created
+
+//Bonus: sashy can be tempermental, too. she gets rid of friends sometimes. give her a method
+// called 'deletesFriend' that takes a friends name and deletes that friend from the favoritePeople list. if it can't
+// find that friend in the list, it logs "Can't find that friend!"
+// Hint: use javaScript's built in 'indexOf method'. read the docs if you're not familiar with it.
+
+//Done!
